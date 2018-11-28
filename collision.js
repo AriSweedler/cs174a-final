3840ccc1ecec4368465d3a90284f06978c971f04
Nikita Lukyanenko <nikita1923666@gmail.com>
2018-11-27 17:05:49 -0800

    Here is what I got so far guys, lets just merge evrything to see where we are

---
+++ collision.js
@@ -0,0 +1,31 @@
+window.CollidingSphere = window.classes.CollidingSphere =
+    class CollidingSphere {
+        constructor(position, rotAngle, rotAxis, radius) {
+            this.position = position;
+            this.rotAngle = rotAngle;
+            this.rotAxis = rotAxis;
+            this.radius = radius;
+        }
+
+        // draw(graphics_state, model, material) {
+        //     //console.log('hello')
+        //     const transform = Mat4.identity()//.times(Mat4.translation([0,1,0]))
+        //         .times(Mat4.translation(this.position))
+        //         .times(Mat4.rotation(this.rotAngle, this.rotAxis))
+        //         .times(Mat4.scale([this.radius, this.radius, this.radius]));
+        //     model.draw(graphics_state, transform, material)
+        // }
+
+        collides(otherSphere) {
+            const diffVec = Vec.of(...this.position).minus(Vec.of(...otherSphere.position));
+            //console.log(diffVec)
+            const dist = Math.sqrt(diffVec.dot(diffVec));
+            return dist <= this.radius + otherSphere.radius
+        }
+
+        translate(x, y, z) {
+            this.position[0] += x;
+            this.position[1] += y;
+            this.position[2] += z
+        }
+    }
