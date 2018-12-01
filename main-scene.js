b8236aa7301a175e216dbffdfa9fa563f4c46b34
Ari Sweedler <ari@sweedler.com>
2018-11-30 16:02:24 -0800

    Added everything for the flashlight except the shader and z-buffer picking

--- main-scene.js
+++ main-scene.js
@@ -1,5 +1,5 @@
-window.Assignment_Three_Scene = window.classes.Assignment_Three_Scene =
-    class Assignment_Three_Scene extends Scene_Component {
+window.Term_Project_Scene = window.classes.Term_Project_Scene =
+    class Term_Project_Scene extends Scene_Component {
         constructor(context, control_box)     // The scene begins by requesting the camera, shapes, and materials it will need.
         {
             super(context, control_box);    // First, include a secondary Scene that provides movement controls:
@@ -13,9 +13,21 @@ window.Assignment_Three_Scene = window.classes.Assignment_Three_Scene =
             const r = context.width / context.height;
             context.globals.graphics_state.projection_transform = Mat4.perspective(Math.PI / 4, r, .1, 1000);
 
+            this.webgl_manager = context;      // Save off the Webgl_Manager object that created the scene.
+            this.scratchpad = document.createElement('canvas');
+            this.scratchpad_context = this.scratchpad.getContext('2d');     // A hidden canvas for re-sizing the real canvas to be square.
+            this.scratchpad.width   = 256;
+            this.scratchpad.height  = 256;
+            this.texture = new Texture ( context.gl, "", false, false );        // Initial image source: Blank gif file
+            this.texture.image.src = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7";
+
+            // TODO:  Create two cubes, including one with the default texture coordinates (from 0 to 1), and one with the modified
+            //        texture coordinates as required for cube #2.  You can either do this by modifying the cube code or by modifying
+            //        a cube instance's texture_coords after it is already created.
             const shapes = {
                 box: new Cube(),
                 box_2: new Cube(),
+                cone: new Rounded_Closed_Cone(10, 10),
                 axis: new Axis_Arrows(),
                 square: new Square(),
                 player: new Subdivision_Sphere(4)
@@ -25,22 +37,26 @@ window.Assignment_Three_Scene = window.classes.Assignment_Three_Scene =
            
             this.submit_shapes(context, shapes);
             this.logic = new Logic();
-            this.materials =
-                {
-                    phong: context.get_instance(Phong_Shader).material(Color.of(1, 1, 0, 1)),
-                    phong2:context.get_instance(Phong_Shader).material(Color.of(1, 1, 1, 1),{ambient: 1, diffuse: 1}),
-                    'wall': context.get_instance(Phong_Shader).material(Color.of(0, 0, 0, 1), {
-                        specularity: 0,
-                        ambient: 0.5,
-                        texture: context.get_instance("assets/stone03b.jpg", false)
-                    }),
-                    'floor': context.get_instance(Phong_Shader).material(Color.of(0, 0, 0, 1), {
-                        specularity: 0,
-                        ambient: 0.7,
-                        texture: context.get_instance("assets/floor.jpg", false)
-                    })
-
-                };
+            this.materials = {
+                phong: context.get_instance(Phong_Shader).material(Color.of(1, 1, 0, 1)),
+                phong2:context.get_instance(Phong_Shader).material(Color.of(1, 1, 1, 1),{ambient: 1, diffuse: 1}),
+                'wall': context.get_instance(Phong_Shader).material(Color.of(0, 0, 0, 1), {
+                    specularity: 0,
+                    ambient: 0.5,
+                    texture: context.get_instance("assets/stone03b.jpg", false)
+                }),
+                'floor': context.get_instance(Phong_Shader).material(Color.of(0, 0, 0, 1), {
+                    specularity: 0,
+                    ambient: 0.7,
+                    texture: context.get_instance("assets/floor.jpg", false)
+                }),
+                flashlight: context.get_instance(Flashlight_Shader).material(Color.of(0, 0, 0, 1), {
+                    // ambient to 1, diffuse to 0, and specular to 0
+                    ambient: 1,
+                    diffusivity: 0,
+                    specularity: 0,
+                })
+            };
 
             this.lights = [new Light(Vec.of(-5, 5, 5, 1), Color.of(0, 1, 1, 1), 100000)];
 
@@ -56,6 +72,15 @@ window.Assignment_Three_Scene = window.classes.Assignment_Three_Scene =
             this.time = 0;
             //this.colliders = [];
             this.nextSpawn = 5.0;
+            this.flashlight = {
+                angle: {
+                    x: 0,
+                    y: 0
+                },
+                centerToTip: [0, 0, 1],
+                playerToHand: [0.1, 0.1, -0.9],
+                longThin: [1, 1, 5]
+            };
         }
 
         make_control_panel() {
@@ -74,9 +99,43 @@ window.Assignment_Three_Scene = window.classes.Assignment_Three_Scene =
             this.key_triggered_button("switchCamera", ["x"], () => {
                 this.camera = !this.camera;
             });
+            this.result_img = this.control_panel.appendChild( 
+                Object.assign( document.createElement( "img" ), {
+                    style:"width:200px; height:" + 200 * this.aspect_ratio + "px"
+                } ) 
+            );
+
+            this.new_line();
+            /*y = 0.8 is as far left, y=-0.8 is as far right*/
+            /*x = 0.44 is top of the screen, x = -0.03 is the bottom of the screen*/
+            this.key_triggered_button("lightLeft", ["a"], () => {
+                if (this.flashlight.angle.y >= 0.8) {
+                    return
+                }
+                this.flashlight.angle.y += 0.01;
+            });
+            this.key_triggered_button("lightRight", ["d"], () => {
+                if (this.flashlight.angle.y <= -0.8) {
+                    return;
+                }
+                this.flashlight.angle.y -= 0.01;
+            });
+            this.key_triggered_button("lightUp", ["w"], () => {
+                if (this.flashlight.angle.x >= 0.5) {
+                    return;
+                }
+                this.flashlight.angle.x += 0.01;
+            });
+            this.key_triggered_button("lightDown", ["s"], () => {
+                if (this.flashlight.angle.x <= 0) {
+                    return;
+                }
+                this.flashlight.angle.x -= 0.01;
+            });
         }
 
         display(graphics_state) {
+            // (Draw scene 1)
             graphics_state.lights = this.lights;        // Use the lights stored in this.lights.
             const t = graphics_state.animation_time / 1000, dt = graphics_state.animation_delta_time / 1000;
             this.time = t;
@@ -108,6 +167,31 @@ window.Assignment_Three_Scene = window.classes.Assignment_Three_Scene =
             } else if (t > this.time + 1) {
                 graphics_state.camera_transform = Mat4.look_at(Vec.of(0, 6, 8), Vec.of(0, 2, 0), Vec.of(0, 1, 0));
             }
+
+            /************************************* flashlight *************************************************/
+            /* Draw light coming from flashlight (player's hand) */
+            this.flashlight.transform = this.playerM.times( Mat4.translation(this.flashlight.playerToHand) );
+
+            /*place the tip of the flashlight where the players hand would be */
+            this.flashlight.transform = this.flashlight.transform.times( Mat4.translation(this.flashlight.playerToHand) );
+
+            /* Rotate the light first */
+            /* angle.x++ moves the light up */
+            /* angle.y++ moves the light left */
+            this.flashlight.transform = this.flashlight.transform
+                .times( Mat4.translation(this.flashlight.centerToTip) )
+                .times( Mat4.rotation(this.flashlight.angle.x, [1, 0, 0]) )
+                .times( Mat4.rotation(this.flashlight.angle.y, [0, 1, 0]) )
+                .times( Mat4.translation(this.flashlight.centerToTip.map(i=>-i)) );
+
+            /* Make the lightcone long and thin (long enough to hit the far wall) */
+            this.flashlight.transform = this.flashlight.transform
+                .times( Mat4.translation(this.flashlight.centerToTip) )
+                .times( Mat4.scale(this.flashlight.longThin) )
+                .times( Mat4.translation(this.flashlight.centerToTip.map(i=>-i)) );
+
+            this.shapes.cone.draw(graphics_state, this.flashlight.transform, this.materials.flashlight);
+            /************************************* flashlight *************************************************/
 
             let grid_transform = Mat4.identity().times(Mat4.rotation(Math.PI / 2, Vec.of(1, 0, 0))).times(Mat4.translation([-7, 0, 0]));
             for (let i = 0; i < this.logic.grid.length; i++) {
@@ -154,71 +238,24 @@ window.Assignment_Three_Scene = window.classes.Assignment_Three_Scene =
                 this.colliders[i].draw(graphics_state, this.shapes.player, this.materials.phong.override({color: this.colliders[i].color}));
                 this.colliders[i].move(t,this.playerPos);
             }
+            // (Save scene 1 into an image)
+            this.scratchpad_context.drawImage( this.webgl_manager.canvas, 0, 0, 256, 256 );
+            this.texture.image.src = this.result_img.src = this.scratchpad.toDataURL("image/png");
+
+            // Clear the canvas and start over, beginning scene 2:
+            // this.webgl_manager.gl.clear( this.webgl_manager.gl.COLOR_BUFFER_BIT | this.webgl_manager.gl.DEPTH_BUFFER_BIT);
+
+            // (Draw scene 2)
         }
     };
 
-class Texture_Scroll_X extends Phong_Shader {
-    fragment_glsl_code()           // ********* FRAGMENT SHADER *********
+class Flashlight_Shader extends Phong_Shader {
+    /*************************** FRAGMENT SHADER ****************************/
+    fragment_glsl_code() {return `
+    uniform sampler2D texture;
+    void main()
     {
-        // TODO:  Modify the shader below (right now it's just the same fragment shader as Phong_Shader) for requirement #6.
-        return `
-        uniform sampler2D texture;
-        void main()
-        { if( GOURAUD || COLOR_NORMALS )    // Do smooth "Phong" shading unless options like "Gouraud mode" are wanted instead.
-          { gl_FragColor = VERTEX_COLOR;    // Otherwise, we already have final colors to smear (interpolate) across vertices.            
-            return;
-          }                                 // If we get this far, calculate Smooth "Phong" Shading as opposed to Gouraud Shading.
-                                            // Phong shading is not to be confused with the Phong Reflection Model.
-          vec4 workingCoords = vec4(f_tex_coord,1.0,1.0);
-          float i = 2.0*(animation_time - 2.0 * floor(animation_time/2.0));
-          mat4 translateM = mat4(1.0, 0.0, 0.0, 0.0,
-                                 0.0, 1.0, 0.0, 0.0,
-                                 0.0, 0.0, 1.0, 0.0,
-                                 i,   0.0,   0.0,   1.0);
-          vec4 translated = translateM*workingCoords;
-          vec2 cut = translated.xy;
-          vec4 tex_color = texture2D( texture, cut );                         // Sample the texture image in the correct place.
-                                                                                      // Compute an initial (ambient) color:
-          if( USE_TEXTURE ) gl_FragColor = vec4( ( tex_color.xyz + shapeColor.xyz ) * ambient, shapeColor.w * tex_color.w ); 
-          else gl_FragColor = vec4( shapeColor.xyz * ambient, shapeColor.w );
-          gl_FragColor.xyz += phong_model_lights( N );                     // Compute the final color with contributions from lights.
-        }`;
+        gl_FragColor = vec4(1, 1, 1, 1);
     }
-}
-
-class Texture_Rotate extends Phong_Shader {
-    fragment_glsl_code()           // ********* FRAGMENT SHADER *********
-    {
-        // TODO:  Modify the shader below (right now it's just the same fragment shader as Phong_Shader) for requirement #7.
-        return `
-        uniform sampler2D texture;
-        void main()
-        { if( GOURAUD || COLOR_NORMALS )    // Do smooth "Phong" shading unless options like "Gouraud mode" are wanted instead.
-          { gl_FragColor = VERTEX_COLOR;    // Otherwise, we already have final colors to smear (interpolate) across vertices.            
-            return;
-          }                                 // If we get this far, calculate Smooth "Phong" Shading as opposed to Gouraud Shading.
-                                            // Phong shading is not to be confused with the Phong Reflection Model.
-          vec4 workingCoords = vec4(f_tex_coord,1.0,1.0);
-          float i = 0.5*3.1415926*(animation_time - 4.0 * floor(animation_time/4.0));
-          mat4 translateM = mat4(1.0, 0.0, 0.0, 0.0,
-                                 0.0, 1.0, 0.0, 0.0,
-                                 0.0, 0.0, 1.0, 0.0,
-                                 -0.5,   -0.5,   0.0,   1.0);
-          mat4 rotateM = mat4(cos(i), sin(i), 0.0, 0.0,
-                                 -sin(i), cos(i), 0.0, 0.0,
-                                 0.0, 0.0, 1.0, 0.0,
-                                 0.0,   0.0,   0.0,   1.0);
-          mat4 invTranslateM = mat4(1.0, 0.0, 0.0, 0.0,
-                                 0.0, 1.0, 0.0, 0.0,
-                                 0.0, 0.0, 1.0, 0.0,
-                                 -0.5,   -0.5,   0.0,   1.0);
-          vec4 translated = invTranslateM*rotateM*translateM*workingCoords;
-          vec2 cut = translated.xy;
-          vec4 tex_color = texture2D( texture, cut );                          // Sample the texture image in the correct place.
-                                                                                      // Compute an initial (ambient) color:
-          if( USE_TEXTURE ) gl_FragColor = vec4( ( tex_color.xyz + shapeColor.xyz ) * ambient, shapeColor.w * tex_color.w ); 
-          else gl_FragColor = vec4( shapeColor.xyz * ambient, shapeColor.w );
-          gl_FragColor.xyz += phong_model_lights( N );                     // Compute the final color with contributions from lights.
-        }`;
-    }
+    `;}
 }
